38d90c3b33efa7c59d9a3c0322458642cf701e7e
pra7084 <pradeepsingh80007@gmail.com>
2023-12-15 03:47:47 +0530

    project completed

--- src/Components/Concepts.jsx
+++ src/Components/Concepts.jsx
@@ -1,67 +1,32 @@
-import comp from "../../src/assets/components.png";
-import jsx from "../../src/assets/jsx-ui.png";
-import props from "../../src/assets/config.png";
-import state from "../../src/assets/state-mgmt.png";
+
 import "./Concepts.css";
 import ConceptDes from "./ConceptDes";
+import DummyData from "./DummyData.jsx";
+import { useState } from "react";
 
 const Concepts = () => {
-  const DummyData = [
-    {
-      img: comp,
-      heading: <p>Components</p>,
-      para: (
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
-          similique iste ipsum repellat omnis aspernatur quos aut distinctio,
-        </p>
-      ),
-    },
-    {
-      img:  jsx,
-      heading: <p>JSX</p>,
-      para: (
-        <p>
-          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
-          reprehenderit aperiam aspernatur. Debitis, totam illo.
-        </p>
-      ),
-    },
-    {
-      img: props,
-      heading: <p>Props</p>,
-      para: (
-        <p>
-          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
-          fuga, exercitationem pariatur sunt laboriosam voluptate!
-        </p>
-      ),
-    },
-    {
-      img:  state ,
-      heading: <p>State</p>,
-      para: (
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ut
-          voluptatum nesciunt dolores, quod libero!
-        </p>
-      ),
-    },
-  ];
+  const [conceptOpener,setConceptOpener]=useState(true);
+
+  const openConceptHandler=()=>{
+     setConceptOpener(true);
+    
+  }
   return (
     <>
       <div className="outer">
         <div className="blackbox">
           <div className="core">Core Concepts</div>
+          {/* {!conceptOpener && <div><button onClick={openConceptHandler}>Open Core Concepts</button></div>} */}
           <div className="conceptdes">
-                 <ConceptDes data={DummyData[0]} />
-          <ConceptDes data={DummyData[1]} />
-           <ConceptDes data={DummyData[2]} />
+            {DummyData.map((item, index) => (
+              <ConceptDes key={index} data={item} />
+            ))}
+
+            {/* // <ConceptDes data={DummyData[1]} /> */}
+            {/* //  <ConceptDes data={DummyData[2]} /> */}
             {/* <ConceptDes data={DummyData[3]} /> */}
-          
           </div>
-     
-         
+        
         </div>
       </div>
     </>
---
+++ src/Components/DummyData.jsx
@@ -0,0 +1,47 @@
+  import comp from "../../src/assets/components.png";
+import jsx from "../../src/assets/jsx-ui.png";
+import props from "../../src/assets/config.png";
+import state from "../../src/assets/state-mgmt.png";
+  const DummyData = [
+    {
+      img: comp,
+      heading: <p>Components</p>,
+      para: (
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Culpa
+          similique iste ipsum repellat omnis aspernatur quos aut distinctio,
+        </p>
+      ),
+    },
+    {
+      img: jsx,
+      heading: <p>JSX</p>,
+      para: (
+        <p>
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Qui
+          reprehenderit aperiam aspernatur. Debitis, totam illo.
+        </p>
+      ),
+    },
+    {
+      img: props,
+      heading: <p>Props</p>,
+      para: (
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Possimus
+          fuga, exercitationem pariatur sunt laboriosam voluptate!
+        </p>
+      ),
+    },
+    {
+      img: state,
+      heading: <p>State</p>,
+      para: (
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam ut
+          voluptatum nesciunt dolores, quod libero!
+        </p>
+      ),
+    },
+  ];
+  export default DummyData;
